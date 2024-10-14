bd56b12bd264143a8d0660a84a0286ab092ee7ea
Sezer <sezer.boluk@niltekyazilim.com.tr>
2024-10-14 15:34:59 +0300

    latests

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,7 +6,7 @@ const config: GatsbyConfig = {
     description: `Ankara Toptan Palet Satışı`,
     twitterUsername: `@karadenizpalet`,
     image:``,
-    siteUrl: `https://karadenizpalet.com/`,
+    siteUrl: `https://karadenizpalet.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
